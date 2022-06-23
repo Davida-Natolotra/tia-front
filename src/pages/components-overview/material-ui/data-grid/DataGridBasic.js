f8a19aaf289094c506ee1002c6767dccfb040b4c
Dina <dina@MacBook-Pro-de-Dina-Ranaivo.local>
2022-06-23 15:14:40 +0300

    Add context basic menu

--- src/pages/components-overview/material-ui/data-grid/DataGridBasic.js
+++ src/pages/components-overview/material-ui/data-grid/DataGridBasic.js
@@ -2,12 +2,17 @@ import { Icon } from '@iconify/react';
 import moreVerticalFill from '@iconify/icons-eva/more-vertical-fill';
 // material
 import { DataGrid } from '@material-ui/data-grid';
-import { Box } from '@material-ui/core';
+import { Box, Menu, Divider, MenuItem, Typography } from '@material-ui/core';
 // utils
-import mockData from '../../../../utils/mock-data';
+import { useRef, useState } from 'react';
 // components
+import shareFill from '@iconify/icons-eva/share-fill';
+import printerFill from '@iconify/icons-eva/printer-fill';
+import archiveFill from '@iconify/icons-eva/archive-fill';
+import downloadFill from '@iconify/icons-eva/download-fill';
+import trash2Outline from '@iconify/icons-eva/trash-2-outline';
 import { MIconButton } from '../../../../components/@material-extend';
-
+import mockData from '../../../../utils/mock-data';
 // ----------------------------------------------------------------------
 
 const columns = [
@@ -52,10 +57,11 @@ const columns = [
     align: 'right',
     sortable: false,
     disableColumnMenu: true,
-    renderCell: () => (
-      <MIconButton>
-        <Box component={Icon} icon={moreVerticalFill} sx={{ width: 20, height: 20 }} />
-      </MIconButton>
+    renderCell: (params) => (
+      // <MIconButton>
+      //   <Box component={Icon} icon={moreVerticalFill} sx={{ width: 20, height: 20 }} />
+      // </MIconButton>
+      <MoreMenuButton id={params.id} />
     )
   }
 ];
@@ -69,4 +75,72 @@ const rows = [...Array(30)].map((_, index) => ({
 
 export default function DataGridBasic() {
   return <DataGrid columns={columns} rows={rows} checkboxSelection disableSelectionOnClick />;
+}
+
+function MoreMenuButton({ id }) {
+  const menuRef = useRef(null);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  console.log(`id ${id}`);
+
+  return (
+    <>
+      <>
+        <MIconButton ref={menuRef} size="large" onClick={handleOpen}>
+          <Icon icon={moreVerticalFill} width={20} height={20} />
+        </MIconButton>
+      </>
+
+      <Menu
+        open={open}
+        anchorEl={menuRef.current}
+        onClose={handleClose}
+        PaperProps={{
+          sx: { width: 200, maxWidth: '100%' }
+        }}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <MenuItem onClick={() => console.log(`Download id ${id}`)}>
+          <Icon icon={downloadFill} width={20} height={20} />
+          <Typography variant="body2" sx={{ ml: 2 }}>
+            Download
+          </Typography>
+        </MenuItem>
+        <MenuItem>
+          <Icon icon={printerFill} width={20} height={20} />
+          <Typography variant="body2" sx={{ ml: 2 }}>
+            Print
+          </Typography>
+        </MenuItem>
+        <MenuItem>
+          <Icon icon={shareFill} width={20} height={20} />
+          <Typography variant="body2" sx={{ ml: 2 }}>
+            Share
+          </Typography>
+        </MenuItem>
+        <MenuItem>
+          <Icon icon={archiveFill} width={20} height={20} />
+          <Typography variant="body2" sx={{ ml: 2 }}>
+            Archive
+          </Typography>
+        </MenuItem>
+
+        <Divider />
+        <MenuItem sx={{ color: 'error.main' }}>
+          <Icon icon={trash2Outline} width={20} height={20} />
+          <Typography variant="body2" sx={{ ml: 2 }}>
+            Delete
+          </Typography>
+        </MenuItem>
+      </Menu>
+    </>
+  );
 }
