15e168de7a384e0241176f9737ade88db81bdcce
Dina <dina@MacBook-Pro-de-Dina-Ranaivo.local>
2022-09-10 12:06:58 +0300

    New release

--- src/redux/slices/moto.js
+++ src/redux/slices/moto.js
@@ -3,8 +3,8 @@ import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { createOrUpdateFromMoto } from './caisse';
 // Endpoint
-export const url = 'http://localhost:8000';
-// export const url = 'https://tiamoto.com/backend';
+// export const url = 'http://localhost:8000';
+export const url = 'https://tiamoto.com/backend';
 // ----------------------------------------------------------------------
 
 const initialState = {
